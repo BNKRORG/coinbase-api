8cfbd634808429b2306a356843ca845aab97661d
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-09-25 10:52:27 -0300

    Add accounts example
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

---
+++ examples/accounts.rs
@@ -0,0 +1,17 @@
+use coinbase_api::app::auth::CoinbaseAuth;
+use coinbase_api::app::client::CoinbaseAppClient;
+
+#[tokio::main]
+async fn main() {
+    let auth = CoinbaseAuth::ApiKeys {
+        api_key: String::from("<api-key>"),
+        secret_key: String::from("<secret-key>"),
+    };
+    let client = CoinbaseAppClient::new(auth, false).unwrap();
+
+    let accounts = client.accounts().await.unwrap();
+
+    for account in accounts {
+        println!("{:#?}", account);
+    }
+}
