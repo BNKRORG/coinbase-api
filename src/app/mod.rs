dbcb50130d52f9cf69712ce792380cf3074ccc4f
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-09-25 08:04:14 -0300

    Init rust library
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

---
+++ src/app/mod.rs
@@ -0,0 +1,5 @@
+//! Coinbase App APIs
+//!
+//! <https://docs.cdp.coinbase.com/coinbase-app/introduction/welcome>
+
+pub mod client;
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! Coinbase APIs
+
+#![forbid(unsafe_code)]
+#![warn(missing_docs)]
+#![warn(clippy::large_futures)]
+#![warn(rustdoc::bare_urls)]
+
+pub mod app;
